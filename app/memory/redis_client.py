e6581cba25eef7b492c5be58b2c5dc5c762ffd04
MukamM <muratowmukam@gmail.com>
2026-01-09 14:49:44 +0000

    Remove old app folder and unnecessary files

--- app/memory/redis_client.py
+++
@@ -1,9 +0,0 @@
-import redis
-from app.core.config import REDIS_HOST, REDIS_PORT, REDIS_PASSWORD
-
-r = redis.Redis(
-    host=REDIS_HOST,
-    port=REDIS_PORT,
-    password=REDIS_PASSWORD,
-    decode_responses=True
-)
